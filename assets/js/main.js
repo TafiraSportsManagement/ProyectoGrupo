c4caa1c738a46758312dae42adc80c38d0bb66d6
JoseJAcosta <jose.acosta104@alu.ulpgc.es>
2017-10-21 19:47:49 +0100

    Modifico el main2 y  main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,13 @@ var registrarUsuario = document.getElementById('registrarUsuario');
 var inicio = document.getElementById('inicio');
 var registro = document.getElementById('registro');
 
+var navDeportes = document.getElementById('navDeportes');
+var navMarcas = document.getElementById('navMarcas');
+var navHombre = document.getElementById('navHombre');
+var navMujer = document.getElementById('navMujer');
+var navNinos = document.getElementById('navNiños');
+var navNovedades = document.getElementById('navNovedades');
+
 
 function mostrarFormularioLogin(event) {
     seccionLogin.style.display = "block";
@@ -14,7 +21,6 @@ function mostrarFormularioLogin(event) {
     var elementoFuente = event.target || event.srcElement;
     console.log(elementoFuente);
 
-    <!-- identificar el elemento -->
     if ( elementoFuente.id == "registro" )
         registrarUsuario.style.display = "block";
     else
@@ -33,3 +39,10 @@ function mostrarFormularioSeleccion() {
     seccionContenedores.style.display = "none";
     seccionSeleccion.style.display = "block";
 }
+
+navDeportes.addEventListener('click', mostrarFormularioSeleccion, false);
+navMarcas.addEventListener('click', mostrarFormularioSeleccion, false);
+navHombre.addEventListener('click', mostrarFormularioSeleccion, false);
+navMujer.addEventListener('click', mostrarFormularioSeleccion, false);
+navNinos.addEventListener('click', mostrarFormularioSeleccion, false);
+navNovedades.addEventListener('click', mostrarFormularioSeleccion, false);
