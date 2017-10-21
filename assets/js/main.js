5807dc66bf22da013550c06b35386b6b47d9af94
JoseJAcosta <jose.acosta104@alu.ulpgc.es>
2017-10-21 19:20:45 +0100

    Modifico el main2 y  main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
 
-function login() {
+var seccionLogin = document.getElementById("seccionLogin");
+var seccionContenedores = document.getElementById("seccionContenedores");
+var seccionSeleccion = document.getElementById('seccionSeleccion');
+var registrarUsuario = document.getElementById('registrarUsuario');
+
+var inicio = document.getElementById('inicio');
+var registro = document.getElementById('registro');
 
 
+function mostrarFormularioLogin(event) {
+    seccionLogin.style.display = "block";
+
+    var elementoFuente = event.target || event.srcElement;
+    console.log(elementoFuente);
+
+    <!-- identificar el elemento -->
+    if ( elementoFuente.id == "registro" )
+        registrarUsuario.style.display = "block";
+    else
+        registrarUsuario.style.display = "none";
+
+    seccionContenedores.style.display = "none";
+    seccionSeleccion.style.display = "none";
+}
+
+inicio.addEventListener('click', mostrarFormularioLogin, false);
+registro.addEventListener('click', mostrarFormularioLogin, false);
+
+
+function mostrarFormularioSeleccion() {
+    seccionLogin.style.display = "none";
+    seccionContenedores.style.display = "none";
+    seccionSeleccion.style.display = "block";
 }
