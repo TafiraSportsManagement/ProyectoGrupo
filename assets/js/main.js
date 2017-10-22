9ec7bafa0b0e87594ad7f43d25c154fe21377b93
JoseJAcosta <jose.acosta104@alu.ulpgc.es>
2017-10-22 10:35:05 +0100

    Modifico "style.css", "main.js" y "main2.html" para incorporar la validacion en el formulario de inicio/registro

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,9 @@ var registrarUsuario = document.getElementById('registrarUsuario');
 
 var inicio = document.getElementById('inicio');
 var registro = document.getElementById('registro');
+var salir = document.getElementById('salir');
+var login = document.getElementById('login')
+var logout = document.getElementById('logout');
 
 var navDeportes = document.getElementById('navDeportes');
 var navMarcas = document.getElementById('navMarcas');
@@ -14,13 +17,13 @@ var navMujer = document.getElementById('navMujer');
 var navNinos = document.getElementById('navNiños');
 var navNovedades = document.getElementById('navNovedades');
 
+var botonLoginRegistro = document.getElementById('submitFormLogin');
+
 
 function mostrarFormularioLogin(event) {
     seccionLogin.style.display = "block";
 
     var elementoFuente = event.target || event.srcElement;
-    console.log(elementoFuente);
-
     if ( elementoFuente.id == registro.id )
         registrarUsuario.style.display = "block";
     else
@@ -46,3 +49,49 @@ navHombre.addEventListener('click', mostrarFormularioSeleccion, false);
 navMujer.addEventListener('click', mostrarFormularioSeleccion, false);
 navNinos.addEventListener('click', mostrarFormularioSeleccion, false);
 navNovedades.addEventListener('click', mostrarFormularioSeleccion, false);
+
+
+function validarFormularioLoginRegistro(event) {
+
+    if ( registrarUsuario.style.display == "block" ) {
+
+        var constraseña = document.getElementById('contraseña');
+        var repContraseña = document.getElementById('repContraseña');
+        if ( contraseña.value != repContraseña.value ) {
+            window.alert('ERROR');
+            return;
+        }
+    }
+
+    logOut(event);
+    restablecerContenido();
+}
+
+botonLoginRegistro.addEventListener('click', validarFormularioLoginRegistro, false);
+
+
+function logOut(event) {
+
+    var elementoFuente = event.target || event.srcElement;
+    if ( elementoFuente.id == salir.id ) {
+        document.getElementById('usuario').value = "";
+        document.getElementById('contraseña').value = "";
+        document.getElementById('repContraseña').value = "";
+
+        logout.style.display = "none";
+        login.style.display = "block";
+    } else {
+        logout.style.display = "block";
+        login.style.display = "none";
+    }
+
+}
+
+salir.addEventListener('click', logOut, false);
+
+
+function restablecerContenido() {
+    seccionLogin.style.display = "none";
+    seccionContenedores.style.display = "block";
+    seccionSeleccion.style.display = "none";
+}
