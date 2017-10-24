857025063fab36840b5fde4762d83c8831c2ac6a
Fernando Canellada <fercanellada@gmail.com>
2017-10-24 20:02:44 +0100

    Split javascript for clean code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,14 @@
 var seccionLogin = document.getElementById("seccionLogin");
 var seccionRegistro = document.getElementById("seccionRegistro");
 var seccionContenedores = document.getElementById("seccionContenedores");
-var seccionSeleccion = document.getElementById('seccionSeleccion');
+
 var registrarUsuario = document.getElementById('registrarUsuario');
 
-var inicio = document.getElementById('inicio');
+var log = document.getElementById('log');
 var registro = document.getElementById('registro');
 var salir = document.getElementById('salir');
 var login = document.getElementById('login');
 var logout = document.getElementById('logout');
-
-var navDeportes = document.getElementById('navDeportes');
-var navMarcas = document.getElementById('navMarcas');
-var navHombre = document.getElementById('navHombre');
-var navMujer = document.getElementById('navMujer');
-var navNinos = document.getElementById('navNiños');
-var navNovedades = document.getElementById('navNovedades');
 
 var botonLoginRegistro = document.getElementById('submitFormLogin');
 
@@ -35,37 +28,37 @@ function mostrarFormularioLogin(event) {
     seccionSeleccion.style.display = "none";
 }
 
-inicio.addEventListener('click', mostrarFormularioLogin, false);
+log.addEventListener('click', mostrarFormularioLogin, false);
 
-// function mostrarFormularioRegistro(event) {
-//     seccionRegistro.style.display = "block";
-//
-//     var elementoFuente1 = event.target || event.srcElement;
-//     if ( elementoFuente1.id == registro.id )
-//         registrarUsuario.style.display = "block";
-//     else
-//         registrarUsuario.style.display = "none";
-//     seccionLogin.style.display = "none";
-//     seccionContenedores.style.display = "none";
-//     seccionSeleccion.style.display = "none";
-// }
+function mostrarFormularioRegistro(event) {
+    seccionRegistro.style.display = "block";
 
-// registro.addEventListener('click', mostrarFormularioRegistro, false);
-
-
-function mostrarFormularioSeleccion() {
+    var elementoFuente1 = event.target || event.srcElement;
+    if ( elementoFuente1.id == registro.id )
+        registrarUsuario.style.display = "block";
+    else
+        registrarUsuario.style.display = "none";
     seccionLogin.style.display = "none";
-    seccionRegistro.style.display = "none";
     seccionContenedores.style.display = "none";
-    seccionSeleccion.style.display = "block";
+    seccionSeleccion.style.display = "none";
 }
 
-navDeportes.addEventListener('click', mostrarFormularioSeleccion, false);
-navMarcas.addEventListener('click', mostrarFormularioSeleccion, false);
-navHombre.addEventListener('click', mostrarFormularioSeleccion, false);
-navMujer.addEventListener('click', mostrarFormularioSeleccion, false);
-navNinos.addEventListener('click', mostrarFormularioSeleccion, false);
-navNovedades.addEventListener('click', mostrarFormularioSeleccion, false);
+registro.addEventListener('click', mostrarFormularioRegistro, false);
+
+
+// function mostrarFormularioSeleccion() {
+//     seccionLogin.style.display = "none";
+//     seccionRegistro.style.display = "none";
+//     seccionContenedores.style.display = "none";
+//     seccionSeleccion.style.display = "block";
+// }
+//
+// navDeportes.addEventListener('click', mostrarFormularioSeleccion, false);
+// navMarcas.addEventListener('click', mostrarFormularioSeleccion, false);
+// navHombre.addEventListener('click', mostrarFormularioSeleccion, false);
+// navMujer.addEventListener('click', mostrarFormularioSeleccion, false);
+// navNinos.addEventListener('click', mostrarFormularioSeleccion, false);
+// navNovedades.addEventListener('click', mostrarFormularioSeleccion, false);
 
 
 function validarFormularioLoginRegistro(event) {
---
+++ assets/js/seleccion.js
@@ -0,0 +1,23 @@
+
+var seccionSeleccion = document.getElementById('seccionSeleccion');
+
+var navDeportes = document.getElementById('navDeportes');
+var navMarcas = document.getElementById('navMarcas');
+var navHombre = document.getElementById('navHombre');
+var navMujer = document.getElementById('navMujer');
+var navNinos = document.getElementById('navNiños');
+var navNovedades = document.getElementById('navNovedades');
+
+function mostrarFormularioSeleccion() {
+    seccionLogin.style.display = "none";
+    seccionRegistro.style.display = "none";
+    seccionContenedores.style.display = "none";
+    seccionSeleccion.style.display = "block";
+}
+
+navDeportes.addEventListener('click', mostrarFormularioSeleccion, false);
+navMarcas.addEventListener('click', mostrarFormularioSeleccion, false);
+navHombre.addEventListener('click', mostrarFormularioSeleccion, false);
+navMujer.addEventListener('click', mostrarFormularioSeleccion, false);
+navNinos.addEventListener('click', mostrarFormularioSeleccion, false);
+navNovedades.addEventListener('click', mostrarFormularioSeleccion, false);
