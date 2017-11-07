f6556eefa96a661d47c58b92ef5dc170e70e5e2b
Fernando Canellada <fercanellada@gmail.com>
2017-11-07 19:43:45 +0000

    Solucion Login Modal

--- assets/js/modalLogin.js
+++ assets/js/modalLogin.js
@@ -1,6 +1,26 @@
 /* var modal = document.getElementById('modalLogin'); */
+var btnCancelLogin = document.getElementById("btnCancelarLogin");
+var userLogin = document.forms["formLoginModal"]["usuarioLogin"];
+var userPass = document.forms["formLoginModal"]["contraseñaLogin"];
+function checkFormLogin(){
+    /*function to check userid & password*/
+
+
+    /*the following code checkes whether the entered userid and password are matching*/
+    if(userLogin.value == "123" && userPass.value == "123")
+    {
+        /*opens the target page while Id & password matches*/
+        onValidarFormularioLogin();
+    }
+    else
+    {
+        alert("Error Password or Username")/*displays error message*/
+    }
+}
+
 
 // When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
     if ( event.target == modalLogin ) {
         cancelarFormularioLogin(event);
@@ -8,7 +28,7 @@ window.onclick = function(event) {
     }
 }
 
-function validarFormularioLogin(event) {
+function onValidarFormularioLogin(event) {
     log.style.display ="none";
     logout.style.display = "block";
     modalLogin.style.display = "none";
@@ -17,10 +37,12 @@ function validarFormularioLogin(event) {
 btnLogin.addEventListener('click', validarFormularioLogin, false);
 
 
-function cancelarFormularioLogin(event) {
+
+function cancelFormularioLogin(event) {
     /* console.log("cancelarFormularioLogin"); */
     modalLogin.style.display = "none";
     logout.style.display = "none";
 
 }
-cancelarLogin.addEventListener('click', cancelarFormularioLogin, false);
+btnCancelLogin.addEventListener('click', cancelFormularioLogin, false);
+
