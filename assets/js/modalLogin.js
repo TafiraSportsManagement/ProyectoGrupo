e8a3eda0d6613e8a12f45f18d6dbaa5781afae09
Fernando Canellada <fercanellada@gmail.com>
2017-12-02 21:16:35 +0000

    AJAX Login

--- assets/js/modalLogin.js
+++ assets/js/modalLogin.js
@@ -7,16 +7,16 @@ function checkFormLogin(){
     /*function to check userid & password*/
 
 
-    /*the following code checkes whether the entered userid and password are matching*/
-    if(userLogin.value == "123" && userPass.value == "123")
-    {
-        /*opens the target page while Id & password matches*/
-        onValidarFormularioLogin();
-    }
-    else
-    {
-        alert("Error Password or Username")/*displays error message*/
-    }
+    // /*the following code checkes whether the entered userid and password are matching*/
+    // if(userLogin.value == "123" && userPass.value == "123")
+    // {
+    //     /*opens the target page while Id & password matches*/
+    //     onValidarFormularioLogin();
+    // }
+    // else
+    // {
+    //     alert("Error Password or Username")/*displays error message*/
+    // }
 }
 
 
@@ -46,4 +46,58 @@ function cancelFormularioLogin() {
 
 }
 btnCancelLogin.addEventListener('click', cancelFormularioLogin, false);
+
+$("#formLoginModal").submit(function(event){
+
+    // Prevent default posting of form - put here to work in case of errors
+    event.preventDefault();
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "php/checkLogin.php",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        onValidarFormularioLogin();
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+});
 
